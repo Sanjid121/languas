c650d74849bddec8ed16d27c65be186c9cbfc8c8
Sanjid121 <160921630+Sanjid121@users.noreply.github.com>
2024-07-10 22:21:30 +0600

    -----

---
+++ Grade Calculation.dart
@@ -0,0 +1,15 @@
+void main() {
+  int marks = 90;
+
+  if (marks >= 95) {
+    print("Grade: A");
+  } else if (marks >= 75) {
+    print("Grade: B");
+  } else if (marks >= 65) {
+    print("Grade: C");
+  } else if (marks >= 55) {
+    print("Grade: D");
+  } else {
+    print("Grade: F");
+  }
+}
