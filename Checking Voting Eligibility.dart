ba3e92af6b45d604f1e6219ce25214f468d94201
Sanjid121 <160921630+Sanjid121@users.noreply.github.com>
2024-07-10 22:00:25 +0600

    -----

---
+++ Checking Voting Eligibility.dart
@@ -0,0 +1,10 @@
+void main() {
+  int age = 17;
+
+  if (age >= 18) {
+    print("You are eligible to vote.");
+  } else {
+    print("You are not eligible to vote.");
+  }
+//  jody 18 necha hoi tahola sha buut deta parba naa upora hola parba
+}
