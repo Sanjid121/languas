4937b2adf1167f71b88977017f83f72e9fad3b72
Sanjid121 <160921630+Sanjid121@users.noreply.github.com>
2024-07-10 21:37:11 +0600

    ---

---
+++ Checking Even or Odd.dart
@@ -0,0 +1,11 @@
+void main() {
+  int number = 2;
+
+  if (number % 2 == 0) {
+    print("The number is even.");
+  } else {
+    print("The number is odd.");
+  }
+  // jody jur numbar lakhi-- The number is even--ar jody bajur lakhi
+  // ---The number is odd.
+}
