bd5c56776825aba1fd1ed9fd19424f360e1692db
Sanjid121 <160921630+Sanjid121@users.noreply.github.com>
2024-07-10 21:27:19 +0600

    -----

--- Checking Positive Negative or Zero.dart
+++ Checking Positive Negative or Zero.dart
@@ -1,9 +1,9 @@
 void main() {
-   double number = 5;
+   double number = 5.50; 
 
   if (number > 5) {
     print("The number is positive.");
-  } else if (number < 5) {
+  } else if (number <= 5) {
     print("The number is negative.");
   } else {
     print("The number is zero.");
