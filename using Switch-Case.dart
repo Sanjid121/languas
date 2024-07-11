50ad96ce02c0ea9429370ed04545a9c8127b3f09
Sanjid121 <160921630+Sanjid121@users.noreply.github.com>
2024-07-11 06:07:40 +0600

    -------

---
+++ using Switch-Case.dart
@@ -0,0 +1,17 @@
+void main() {
+  String grade = ("");
+  switch (grade) {
+    case 'A':
+      print("GOOD");
+      break;
+    case 'A-':
+      print('not bet');
+      break;
+    case 'C':
+      print("try better");
+      break;
+    default:
+      print('f');
+  }
+  // bujta parcee 
+}
