8010a8dfb610562ca8ee54a9e14d58bbc2cd4043
Sanjid121 <160921630+Sanjid121@users.noreply.github.com>
2024-07-11 06:41:17 +0600

    ----

---
+++ Sum of First N Natural Numbers using For Loop.dart
@@ -0,0 +1,16 @@
+
+void main() {
+  int n = 5;
+  int sum = 1;
+
+  for (int i = 1; i <= n; i++) {
+    sum += i;
+  }
+
+  print("Sum of first $n natural numbers is $sum");
+ // bujta parce
+}
+
+
+
+
