ca0d14b9ee5aba40105237d85da452806c3aff01
Sanjid121 <160921630+Sanjid121@users.noreply.github.com>
2024-07-27 15:09:49 +0600

    fancstion ues

--- Grade Calculation.dart
+++ Grade Calculation.dart
@@ -11,5 +11,26 @@ void main() {
     print("Grade: D");
   } else {
     print("Grade: F");
+  //FUNCSTION UES
+  name(int marks) {
+    if (marks >= 90) {
+      print("Grade: A+");
+    } else if (marks >= 75) {
+      print("Grade: A");
+    } else if (marks >= 65) {
+      print("Grade: B");
+    } else if (marks >= 55) {
+      print("Grade: D");
+    } else {
+      print("Grade: F");
+    }
   }
+
+  name(50);
+  name(60);
+  name(70);
+  name(80);
+  name(90);
+ 
+
 }
