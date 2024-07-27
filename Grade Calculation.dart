2d29ea6ee7626776a02e6c8c56991dd574616b27
Sanjid121 <160921630+Sanjid121@users.noreply.github.com>
2024-07-27 15:16:26 +0600

    Update Grade Calculation.dart

--- Grade Calculation.dart
+++ Grade Calculation.dart
@@ -1,16 +1,5 @@
 void main() {
-  int marks = 90;
-
-  if (marks >= 95) {
-    print("Grade: A");
-  } else if (marks >= 75) {
-    print("Grade: B");
-  } else if (marks >= 65) {
-    print("Grade: C");
-  } else if (marks >= 55) {
-    print("Grade: D");
-  } else {
-    print("Grade: F");
+  
   //FUNCSTION UES
   name(int marks) {
     if (marks >= 90) {
