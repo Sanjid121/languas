952c7564c0610cddf902b50f4fe33b43f06ab501
Sanjid121 <160921630+Sanjid121@users.noreply.github.com>
2024-07-27 14:49:59 +0600

    funcstion

---
+++ funcstion use if else.dart
@@ -0,0 +1,20 @@
+void main() {
+// function use
+
+  name(int age) {
+    if (age <= 10) {
+      print(" praymari School student class 1-5 ");
+    } else if (age <= 16) {
+      print("School student class 6-10 ");
+    } else if (age <= 21) {
+      print("College student");
+    } else {
+      print("Versity student");
+    }
+  }
+
+  name(10);
+  name(15);
+  name(20);
+  name(25);
+}
